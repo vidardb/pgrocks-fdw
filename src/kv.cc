e55b4e1688b5b4a14e073d49014490c36e743b9d
shichao <jsc0218@gmail.com>
2019-09-24 23:19:55 -0400

    refine kc.h & kv.cc

--- src/kv.cc
+++ src/kv.cc
@@ -8,8 +8,6 @@ using namespace std;
 
 extern "C" {
 
-#include "postgres.h"
-
 void* Open(char* path) {
     DB* db = nullptr;
     Options options;
@@ -26,7 +24,7 @@ void Close(void* db) {
     }
 }
 
-uint64_t Count(void* db) {
+uint64 Count(void* db) {
     string num;
     static_cast<DB*>(db)->GetProperty("rocksdb.estimate-num-keys", &num);
     return stoull(num);
@@ -44,8 +42,8 @@ void DelIter(void* it) {
     }
 }
 
-bool Next(void* db, void* iter, char** key, unsigned int *keyLen,
-          char** value, unsigned int *valLen) {
+bool Next(void* db, void* iter, char** key, uint32* keyLen,
+          char** value, uint32* valLen) {
     Iterator* it = static_cast<Iterator*>(iter);
     if (!it->Valid()) return false;
     *keyLen = it->key().size(), *valLen = it->value().size();
@@ -57,23 +55,23 @@ bool Next(void* db, void* iter, char** key, unsigned int *keyLen,
     return true;
 }
 
-bool Get(void* db, char* key, char** value) {
+bool Get(void* db, char* key, char** value, uint32* valLen) {
     string sval;
     Status s = static_cast<DB*>(db)->Get(ReadOptions(), key, &sval);
     if (!s.ok()) return false;
-    *value = (char*) palloc0(sval.length()+1);
-    strncpy(*value, sval.c_str(), sval.length());
+    *valLen = sval.length();
+    *value = (char*) palloc0(*valLen);
+    strncpy(*value, sval.c_str(), *valLen);
     return true;
 }
 
-bool Put(void* db, char* key, unsigned int keyLen,
-         char* value, unsigned int valLen) {
+bool Put(void* db, char* key, uint32 keyLen, char* value, uint32 valLen) {
     Status s = static_cast<DB*>(db)->Put(WriteOptions(), Slice(key, keyLen),
                                          Slice(value, valLen));
     return s.ok()? true: false;
 }
 
-bool Delete(void* db, char* key, unsigned int keyLen) {
+bool Delete(void* db, char* key, uint32 keyLen) {
     Status s = static_cast<DB*>(db)->Delete(WriteOptions(), Slice(key, keyLen));
     return s.ok()? true: false;
 }
--- src/kv.h
+++ src/kv.h
@@ -6,8 +6,9 @@
 extern "C" {
 #endif
 
-#include <stdint.h>
-#include <stdbool.h>
+//#include <stdint.h>
+//#include <stdbool.h>
+#include "postgres.h"
 
 /**
  * C wrapper
@@ -16,17 +17,16 @@ extern "C" {
 void* Open(char* path);
 void Close(void* db);
 
-uint64_t Count(void* db);
+uint64 Count(void* db);
 
 void* GetIter(void* db);
 void DelIter(void* it);
-bool Next(void* db, void* iter, char** key, unsigned int *keyLen,
-          char** value, unsigned int *valLen);
+bool Next(void* db, void* iter, char** key, uint32* keyLen,
+          char** value, uint32* valLen);
 
-bool Get(void* db, char* key, char** value);
-bool Put(void* db, char* key, unsigned int keyLen,
-         char* value, unsigned int valLen);
-bool Delete(void* db, char* key, unsigned int keyLen);
+bool Get(void* db, char* key, char** value, uint32* valLen);
+bool Put(void* db, char* key, uint32 keyLen, char* value, uint32 valLen);
+bool Delete(void* db, char* key, uint32 keyLen);
 
 
 #if defined(__cplusplus)

1eb962aabd15267e9f09f920a0ee43546bcc56e1
chenquanzhao <quanzhao.cqz@gmail.com>
2020-12-26 13:54:31 +0800

    Update copy and ddl test script

--- sql/testcopy.sql
+++ sql/testcopy.sql
@@ -5,7 +5,7 @@
 \c kvtest
 
 CREATE FOREIGN TABLE product(name VARCHAR(20), make CHAR(50), product_id UUID) SERVER kv_server;
-\copy product FROM 'test/sql/products.csv' WITH CSV;
+\copy product FROM 'sql/products.csv' WITH CSV;
 COPY (SELECT * FROM product) TO '/tmp/products.csv' WITH (FORMAT CSV);
 DROP FOREIGN TABLE product;
-\! diff -uN /tmp/products.csv test/sql/products.csv
+\! diff -uN /tmp/products.csv sql/products.csv

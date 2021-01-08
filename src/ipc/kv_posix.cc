a18b131a35189437e9a8e102aee84d79e5b0c6ca
jsc0218 <jsc0218@gmail.com>
2021-01-08 10:58:49 -0500

    add strerror in ipc

--- src/ipc/kv_posix.cc
+++ src/ipc/kv_posix.cc
@@ -23,6 +23,7 @@ extern "C" {
 int ShmOpen(const char* name, int flag, mode_t mode, const char* func) {
     int fd = shm_open(name, flag, mode);
     if (fd == -1) {
+        pg_fprintf(stderr, "%s\n", pg_strerror(errno));
         ereport(ERROR, errmsg("%s %s failed", func, __func__));
     }
     return fd;
@@ -39,6 +40,7 @@ void* Mmap(void* addr, size_t len, int prot, int flag, int fd, off_t offset,
            const char* func) {
     void* ptr = mmap(addr, len, prot, flag, fd, offset);
     if (ptr == MAP_FAILED) {
+        pg_fprintf(stderr, "%s\n", pg_strerror(errno));
         ereport(ERROR, errmsg("%s %s failed", func, __func__));
     }
     return ptr;
@@ -46,18 +48,21 @@ void* Mmap(void* addr, size_t len, int prot, int flag, int fd, off_t offset,
 
 void Munmap(void* addr, size_t len, const char* func) {
     if (munmap(addr, len) == -1) {
+        pg_fprintf(stderr, "%s\n", pg_strerror(errno));
         ereport(ERROR, errmsg("%s %s failed", func, __func__));
     }
 }
 
 void Ftruncate(int fd, off_t length, const char* func) {
     if (ftruncate(fd, length) == -1) {
+        pg_fprintf(stderr, "%s\n", pg_strerror(errno));
         ereport(ERROR, errmsg("%s %s failed", func, __func__));
     }
 }
 
 void Fclose(int fd, const char* func) {
     if (close(fd) == -1) {
+        pg_fprintf(stderr, "%s\n", pg_strerror(errno));
         ereport(ERROR, errmsg("%s %s failed", func, __func__));
     }
 }
@@ -65,18 +70,21 @@ void Fclose(int fd, const char* func) {
 void SemInit(volatile sem_t* sem, int pshared, unsigned int value,
              const char* func) {
     if (sem_init((sem_t*) sem, pshared, value) == -1) {
+        pg_fprintf(stderr, "%s\n", pg_strerror(errno));
         ereport(ERROR, errmsg("%s %s failed", func, __func__));
     }
 }
 
 void SemDestroy(volatile sem_t* sem, const char* func) {
     if (sem_destroy((sem_t*) sem) == -1) {
+        pg_fprintf(stderr, "%s\n", pg_strerror(errno));
         ereport(ERROR, errmsg("%s %s failed", func, __func__));
     }
 }
 
 void SemPost(volatile sem_t* sem, const char* func) {
     if (sem_post((sem_t*) sem) == -1) {
+        pg_fprintf(stderr, "%s\n", pg_strerror(errno));
         ereport(ERROR, errmsg("%s %s failed", func, __func__));
     }
 }
@@ -86,6 +94,7 @@ int SemWait(volatile sem_t* sem, const char* func) {
         if (errno == EINTR) {
             return -1;
         }
+        pg_fprintf(stderr, "%s\n", pg_strerror(errno));
         ereport(ERROR, errmsg("%s %s failed", func, __func__));
     }
     return 0;
@@ -94,6 +103,7 @@ int SemWait(volatile sem_t* sem, const char* func) {
 int SemTryWait(volatile sem_t* sem, const char* func) {
     int ret = sem_trywait((sem_t*) sem);
     if (ret == -1 && errno != EAGAIN) {
+        pg_fprintf(stderr, "%s\n", pg_strerror(errno));
         ereport(ERROR, errmsg("%s %s failed", func, __func__));
     }
     return ret;

1a1f6ac050d38a3eaf54f190edb4a2f5e3f159c3
Weitian Xing <xingweitian@gmail.com>
2021-08-19 02:05:52 -0400

    first commit.

---
+++ scripts/pgrocks_fdw_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z "$1" ]]; then
+    PG_CONFIG=/usr/local/pgsql/bin/pg_config make
+    sudo PG_CONFIG=/usr/local/pgsql/bin/pg_config make install
+else
+    PG_CONFIG=/usr/local/pgsql/bin/pg_config VIDARDB=true make
+    sudo PG_CONFIG=/usr/local/pgsql/bin/pg_config VIDARDB=true make install
+fi
+
+sudo bash -c 'echo "shared_preload_libraries = 'kv_am'" >> /usr/local/pgsql/data/postgresql.conf'
+sudo -u postgres /usr/local/pgsql/bin/pg_ctl -U postgres -D /usr/local/pgsql/data -l logfile stop
+sudo -u postgres /usr/local/pgsql/bin/pg_ctl -U postgres -D /usr/local/pgsql/data -l logfile start

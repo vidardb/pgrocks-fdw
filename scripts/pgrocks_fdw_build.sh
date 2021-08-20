1b77074a21a44f37049701f051fbb5dcf79d3e02
Weitian Xing <xingweitian@gmail.com>
2021-08-20 01:21:56 -0400

    tweaks.

--- scripts/pgrocks_fdw_build.sh
+++ scripts/pgrocks_fdw_build.sh
@@ -10,6 +10,6 @@ else
     sudo PG_CONFIG=/usr/local/pgsql/bin/pg_config VIDARDB=true make install
 fi
 
-sudo bash -c 'echo "shared_preload_libraries = 'kv_am'" >> /usr/local/pgsql/data/postgresql.conf'
+sudo bash -c 'echo "shared_preload_libraries = 'kv_fdw'" >> /usr/local/pgsql/data/postgresql.conf'
 sudo -u postgres /usr/local/pgsql/bin/pg_ctl -U postgres -D /usr/local/pgsql/data -l logfile stop
 sudo -u postgres /usr/local/pgsql/bin/pg_ctl -U postgres -D /usr/local/pgsql/data -l logfile start

2d0bd0dc8b1d311d9a2593970cbebc597181d2d3
Weitian Xing <xingweitian@gmail.com>
2022-09-24 12:31:30 -0400

    Fix ci build. (#44)

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install -y gcc g++ clang flex libbison-dev libxml2-dev zlib1g-dev libreadline-dev m4 cmake build-essential git wget
 sudo tee /etc/apt/sources.list.d/pgdg.list << END
-deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main
+deb http://apt.postgresql.org/pub/repos/apt/ focal-pgdg main
 END
 wget https://www.postgresql.org/media/keys/ACCC4CF8.asc
 sudo apt-key add ACCC4CF8.asc
--- scripts/vidardb_build.sh
+++ scripts/vidardb_build.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [[ -z "${VIDARDB_VERSION}" ]]; then
-  git clone -b master https://github.com/vidardb/vidardb-engine.git
+  git clone -b master https://"${ACCESS_TOKEN}"@github.com/vidardb/vidardb-engine.git
 else
-  git clone -b v"${VIDARDB_VERSION}" https://github.com/vidardb/vidardb-engine.git
+  git clone -b v"${VIDARDB_VERSION}" https://"${ACCESS_TOKEN}"@github.com/vidardb/vidardb-engine.git
 fi
 
 (cd vidardb-engine/ && sudo DEBUG_LEVEL=0 make shared_lib install-shared -j`nproc`)
